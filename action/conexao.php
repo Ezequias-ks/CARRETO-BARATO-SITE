0ce84488ea5720909192538651a703ba9cf8d3b7
Adelto Oliveira Almeida <adtguitar@gmail.com>
2019-04-01 04:08:54 -0300

    delete to Reade

--- action/conexao.php
+++ action/conexao.php
@@ -7,6 +7,7 @@ $db_name ="carretobarato";
 
 //CONEXAO COM A BASE DE DADOS
 $connect = mysqli_connect($server_name, $user_name, $password_name, $db_name);
+mysqli_set_charset($connect,"utf-8");
 if(mysqli_connect_error()){
     echo "ERROR em se conectar ao Banco: ".mysqli_connect_error();
 }
